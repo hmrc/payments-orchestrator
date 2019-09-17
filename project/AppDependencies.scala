361aa74bf4c94382bf169ca9a9052217c49933bc
Andrew Richards <42971995+andrewrichards1@users.noreply.github.com>
2019-09-17 09:31:28 +0100

    [AJER][OPS-3313] Initial build (#1)
    * [AJER][OPS-3313] Initial build
    
    * [AJER][OPS-3313] Initial build

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -1,21 +1,21 @@
 import play.core.PlayVersion.current
-import play.sbt.PlayImport._
-import sbt.Keys.libraryDependencies
 import sbt._
 
 object AppDependencies {
 
   val compile = Seq(
 
-    "uk.gov.hmrc"             %% "bootstrap-play-26"        % "1.0.0"
+    "uk.gov.hmrc" %% "bootstrap-play-26" % "1.0.0"
   )
 
   val test = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-play-26"        % "1.0.0" % Test classifier "tests",
-    "org.scalatest"           %% "scalatest"                % "3.0.8"                 % "test",
-    "com.typesafe.play"       %% "play-test"                % current                 % "test",
-    "org.pegdown"             %  "pegdown"                  % "1.6.0"                 % "test, it",
-    "org.scalatestplus.play"  %% "scalatestplus-play"       % "3.1.2"                 % "test, it"
+    "uk.gov.hmrc" %% "bootstrap-play-26" % "1.0.0" % Test classifier "tests",
+    "org.scalatest" %% "scalatest" % "3.0.8" % Test,
+    "org.jsoup" % "jsoup" % "1.10.2" % Test,
+    "com.typesafe.play" %% "play-test" % current % Test,
+    "org.pegdown" % "pegdown" % "1.6.0" % Test,
+    "org.scalatestplus.play" %% "scalatestplus-play" % "3.1.2" % Test,
+    "com.github.tomakehurst" % "wiremock-jre8" % "2.21.0" % Test
   )
 
 }

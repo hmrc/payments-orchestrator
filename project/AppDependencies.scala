a365d1a7e74ae512096d9aca85b5ae4f5ceb21a0
ed1286 <ed.taylor@digital.hmrc.gov.uk>
2020-06-19 09:35:52 +0100

    -dependency upgrades

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -1,23 +1,22 @@
-import play.core.PlayVersion.current
+import play.core.PlayVersion
+import play.sbt.PlayImport.ws
 import sbt._
-
 object AppDependencies {
-
   val compile = Seq(
-
+    ws,
+    "uk.gov.hmrc" %% "simple-reactivemongo" % "7.27.0-play-26",
     "uk.gov.hmrc" %% "bootstrap-play-26" % "1.8.0",
-    "uk.gov.hmrc" %% "auth-client" % "3.0.0-play-26"
-    
+    "uk.gov.hmrc" %% "order-id-encoder" % "1.1.0",
+    "com.beachape" %% "enumeratum" % "1.6.0",
+    "uk.gov.hmrc" %% "auth-client" % "3.0.0-play-26",
+    "uk.gov.hmrc" %% "play-hmrc-api" % "4.1.0-play-26"
   )
-
   val test = Seq(
-    "uk.gov.hmrc" %% "bootstrap-play-26" % "1.8.0" % Test classifier "tests",
-    "org.scalatest" %% "scalatest" % "3.0.8" % Test,
-    "org.jsoup" % "jsoup" % "1.10.2" % Test,
-    "com.typesafe.play" %% "play-test" % current % Test,
-    "org.pegdown" % "pegdown" % "1.6.0" % Test,
-    "org.scalatestplus.play" %% "scalatestplus-play" % "3.1.2" % Test,
-    "com.github.tomakehurst" % "wiremock-jre8" % "2.21.0" % Test
+    "org.scalatest" %% "scalatest" % "3.0.5",
+    "org.scalatestplus.play" %% "scalatestplus-play" % "3.1.2",
+    "org.pegdown" % "pegdown" % "1.6.0",
+    "org.mockito" % "mockito-core" % "2.23.0",
+    "com.github.tomakehurst" % "wiremock-jre8" % "2.21.0",
+    "com.typesafe.play" %% "play-test" % PlayVersion.current
   )
-
 }

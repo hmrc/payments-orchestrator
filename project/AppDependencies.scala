1f185d1a9519adb5643177cd0f4e94ffce982eba
Alexis Morcrette <26277447+almorcrette@users.noreply.github.com>
2023-07-31 14:14:52 +0100

    [AM][OPS-10527] move bootstrap-test-play-28 to test dependency

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -8,13 +8,13 @@ object AppDependencies {
   val compile = Seq(
     ws,
     "uk.gov.hmrc" %% "bootstrap-backend-play-28" % boostrapVersion,
-    "uk.gov.hmrc" %% "bootstrap-test-play-28" % boostrapVersion
   )
   val test = Seq(
     "org.scalatest" %% "scalatest" % "3.1.0",
     "org.scalatestplus.play" %% "scalatestplus-play" % "5.1.0",
     "com.github.tomakehurst" % "wiremock-jre8" % "2.27.2",
     "com.typesafe.play" %% "play-test" % "2.8.8",
-    "com.vladsch.flexmark" % "flexmark-all" % "0.35.10"
+    "com.vladsch.flexmark" % "flexmark-all" % "0.35.10",
+    "uk.gov.hmrc" %% "bootstrap-test-play-28" % boostrapVersion
   ).map(_ % Test)
 }

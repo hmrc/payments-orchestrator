9019db5db416000b705e63d3266f2793d879750e
Jack Gilman <56401347+DreamcastKnight@users.noreply.github.com>
2024-03-21 08:44:32 +0000

    OPS-11436 - upgrade Playframework to v3.0 (#34)
    * [JG] OPS-11436 - Started play upgrade
    
    * [JG] OPS-11436 - continuing play upgrade including test changes
    
    * [JG] OPS-11436 - continuing play upgrade including test changes
    
    * [JG] OPS-11436 - removed redundant line

--- build.sbt
+++ build.sbt
@@ -2,13 +2,13 @@ import uk.gov.hmrc.DefaultBuildSettings.{defaultSettings, integrationTestSetting
 import wartremover.WartRemover.autoImport.{wartremoverErrors, wartremoverExcluded}
 
 
-val scalaV = "2.13.12"
-scalaVersion := scalaV
+scalaVersion := "2.13.12"
 
 lazy val microservice = Project(appName, file("."))
   .enablePlugins(play.sbt.PlayScala, SbtAutoBuildPlugin, SbtGitVersioning, SbtDistributablesPlugin)
   .settings(
     libraryDependencies ++= AppDependencies.compile ++ AppDependencies.test,
+    libraryDependencySchemes ++= Seq("org.scala-lang.modules" %% "scala-xml" % VersionScheme.Always),
     retrieveManaged := false,
     routesGenerator := InjectedRoutesGenerator,
     update / evictionWarningOptions := EvictionWarningOptions.default.withWarnScalaVersionEviction(warnScalaVersionEviction = false)
@@ -28,7 +28,6 @@ lazy val microservice = Project(appName, file("."))
   .settings(PlayKeys.playDefaultPort := 8418)
   .settings(scalaSettings *)
   .settings(defaultSettings() *)
-  .settings(integrationTestSettings())
   .configs(IntegrationTest)
   .settings(
     routesImport ++= Seq(
--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,18 +3,18 @@ import sbt.*
 
 object AppDependencies {
 
-  val boostrapVersion = "7.22.0"
+  val boostrapVersion = "8.5.0"
   val compile = Seq(
     ws,
-    "uk.gov.hmrc"                   %% "bootstrap-backend-play-28"  % boostrapVersion,
+    "uk.gov.hmrc"                   %% "bootstrap-backend-play-30"  % boostrapVersion,
     "com.fasterxml.jackson.module"  %% "jackson-module-scala"       % "2.15.2"
   )
   val test = Seq(
     "org.scalatest"           %% "scalatest"                        % "3.2.17",
     "org.scalatestplus.play"  %% "scalatestplus-play"               % "5.1.0",
     "org.wiremock"            %  "wiremock-standalone"              % "3.2.0",
-    "com.typesafe.play"       %% "play-test"                        % "2.8.20",
+    "org.playframework"       %% "play-test"                        % "2.8.20",
     "com.vladsch.flexmark"    %  "flexmark-all"                     % "0.64.8",
-    "uk.gov.hmrc"             %% "bootstrap-test-play-28"           % boostrapVersion
+    "uk.gov.hmrc"             %% "bootstrap-test-play-30"           % boostrapVersion
   ).map(_ % Test)
 }

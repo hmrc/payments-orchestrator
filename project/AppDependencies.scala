276910113db587add91bd69cbb461614a55eca7f
Andrew Chung <26929776+AndyHWChung@users.noreply.github.com>
2026-06-19 10:45:50 +0100

    [AC][OPS-15143] bump up play (#43)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -7,13 +7,13 @@ object AppDependencies {
   val compile: Seq[ModuleID] = Seq(
     ws,
     "uk.gov.hmrc"                   %% "bootstrap-backend-play-30"  % boostrapVersion,
-    "com.fasterxml.jackson.module"  %% "jackson-module-scala"       % "2.21.0"
+    "com.fasterxml.jackson.module"  %% "jackson-module-scala"       % "2.22.0"
   )
   val test: Seq[ModuleID] = Seq(
-    "org.scalatest"           %% "scalatest"                        % "3.2.19",
+    "org.scalatest"           %% "scalatest"                        % "3.2.20",
     "org.scalatestplus.play"  %% "scalatestplus-play"               % "7.0.2",
     "org.wiremock"            %  "wiremock-standalone"              % "3.13.2",
-    "org.playframework"       %% "play-test"                        % "3.0.10",
+    "org.playframework"       %% "play-test"                        % "3.0.11",
     "com.vladsch.flexmark"    %  "flexmark-all"                     % "0.64.8",
     "uk.gov.hmrc"             %% "bootstrap-test-play-30"           % boostrapVersion
   ).map(_ % Test)

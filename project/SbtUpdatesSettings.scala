61b126260fdcc815ba4685d2ddac9ace09087138
JamieNorth <75728919+JamieNorth@users.noreply.github.com>
2026-09-07 09:06:42 +0100

    [JN][OPS-15366] - Set up SbtUpdates

--- project/SbtUpdatesSettings.scala
+++ project/SbtUpdatesSettings.scala
@@ -8,12 +8,7 @@ object SbtUpdatesSettings {
   lazy val sbtUpdatesSettings: Seq[Def.Setting[?]] = Seq(
     dependencyUpdatesFailBuild := true,
     (Compile / compile) := ((Compile / compile) dependsOn dependencyUpdates).value,
-    dependencyUpdatesFilter -= moduleFilter("org.scala-lang"),
-    dependencyUpdatesFilter -= moduleFilter("org.playframework"),
-    // remove after upgrading to 13.0.0
-    dependencyUpdatesFilter -= moduleFilter("uk.gov.hmrc", "play-frontend-hmrc-play-30"),
-    // locked by version of play
-    dependencyUpdatesFilter -= moduleFilter("org.scalatestplus.play", "scalatestplus-play")
+    dependencyUpdatesFilter -= moduleFilter("org.playframework")
 
   )
 
